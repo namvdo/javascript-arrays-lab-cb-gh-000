ef930e32be7abc1f46836a1107c50a0d97016f93
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-19 10:13:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  var removeSomethingFromtheLast = kittens.slice(0, kittens.length);
+  var removeSomethingFromtheLast = kittens.pop();
   return removeSomethingFromtheLast;
 }
