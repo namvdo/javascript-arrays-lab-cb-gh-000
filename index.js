235907f3d531f854c6fc3290784b95b16593d8db
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-19 09:58:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-return    kittens.push(name);
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
 }
